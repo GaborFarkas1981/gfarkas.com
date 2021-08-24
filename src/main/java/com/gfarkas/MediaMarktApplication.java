fbe4f33a7b88edf4feeca3567d449246a44368a7
gFarkas1981 <info@gfarkas.com>
2021-08-24 13:04:07 +0200

    - daos - SecurityConfig for h2 db

--- src/main/java/com/gfarkas/MediaMarktApplication.java
+++ src/main/java/com/gfarkas/MediaMarktApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SecuringWebApplication {
+public class MediaMarktApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SecuringWebApplication.class, args);
+		SpringApplication.run(MediaMarktApplication.class, args);
 	}
 
 }
